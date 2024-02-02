1b5057940b0d2af8a91e4ceebcea24481997ac2b
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2024-02-02 23:17:30 +0000

    added comments to file

--- RESTfulAPI/db/mongo.js
+++ RESTfulAPI/db/mongo.js
@@ -1,18 +1,26 @@
 import mongoose from "mongoose"
 
+// Load environment variables from the .env file
 import 'dotenv/config'
 
+
+// Function to connect to MongoDB using the specified URI
 export async function connectMongoDB(){
-    const uri = `mongodb+srv://kashuliy:${process.env.MONGODB_PASSWORD}@justhavingfundb.ukibmdr.mongodb.net/?retryWrites=true&w=majority`;
-    
+    // Construct the MongoDB connection URI with credentials from environment variables
+    const uri = `mongodb+srv://kashuliy:${process.env.MONGODB_PASSWORD}@justhavingfundb.ukibmdr.mongodb.net/?retryWrites=true&w=majority`
     try{
-        await mongoose.connect(uri);
-        console.log("Connected to MongoDB");
+        // Attempt to connect to MongoDB using the constructed URI
+        await mongoose.connect(uri)
+
+        console.log("Connected to MongoDB")
+
     } catch (error){
-        console.error(error);
+        // Log an error message if the connection attempt fails
+        console.error(error)
     }
 }
 
+// Define the schema for the User collection in MongoDB
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -29,5 +37,8 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// Create and export the User model based on the defined schema
 export const userModel = mongoose.model('User', userSchema)
+
+// Export the connectMongoDB function (used for establishing the database connection)
 export default connectMongoDB
